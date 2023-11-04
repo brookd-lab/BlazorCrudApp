61ee0266cd4d77d3ce3a82b5b7dc58c0016fe89c
David Brook <brookd2022@gmail.com>
2023-11-04 14:06:47 -0700

    Add project files.

---
+++ Shared/Repo/IDeveloperRepository.cs
@@ -0,0 +1,18 @@
+﻿using BlazorCrudApp.Shared.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorCrudApp.Shared.Repo
+{
+    public interface IDeveloperRepository
+    {
+        public Task<List<Developer>> GetAll();
+        public Task<Developer?> GetById(int id);
+        public Task<Developer?> Insert(Developer developer);
+        public Task<Developer> Update(Developer developer);
+        public Task<Developer?> Remove(int id);
+    }
+}
